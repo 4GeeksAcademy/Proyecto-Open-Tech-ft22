72288d7e8d61402905c429c58ec00ca80bb4798b
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-17 18:05:39 +0000

    added logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import OpenTechLogo from "../../img/key-icon.png";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1"><img className="openTech-Logo" src={OpenTechLogo}/>OpenTech Insights</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
