2b8d4aa0e5deebe0c41cc8550d51465f2356c177
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-27 18:13:31 +0000

    added 5 categories and 10 roles per cat

--- src/front/js/component/roleCard.js
+++ src/front/js/component/roleCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const RoleCard = ({ roleName, average, entries }) => {
     return (
-        <div className="card salaryCard">
+        <div className="card salaryCard" style={{ height: '140px' }}>
             <div className="card-body">
-                <h5 className="card-title">{roleName}</h5>
-                <p className="card-subtitle mb-2 text-body-secondary">Average: ${average}M</p>
-                <p className="card-subtitle mb-2 text-body-secondary">Entries: {entries}</p>
+                <h5 className="card-title" style={{ fontSize: '18px' }}>{roleName}</h5>
+                <p className="card-subtitle mb-2 text-body-secondary" style={{ fontSize: '17px' }}>Average: ${average}M</p>
+                <p className="card-subtitle mb-2 text-body-secondary" style={{ fontSize: '15px' }}>Entries: {entries}</p>
             </div>
         </div>
     )
--- src/front/js/pages/mainroles.js
+++ src/front/js/pages/mainroles.js
@@ -16,11 +16,11 @@ export default MainRoles;
 
 
 const Tab = styled.button`
-  font-size: 20px;
+  font-size: 15px;
   padding: 10px 60px;
   cursor: pointer;
   opacity: 0.6;
-  background: white;
+  background: grey;
   border: 0;
   outline: 0;
   ${({ active }) =>
@@ -28,14 +28,37 @@ const Tab = styled.button`
         `
     border-bottom: 2px solid black;
     opacity: 1;
+    color: purple;
   `}
 `;
 const ButtonGroup = styled.div`
   display: flex;
 `;
-const types = ['Cash', 'Credit Card', 'Bitcoin'];
+
+// Mapping of tab types to their roles
+const roles = {
+    'Software Development': ['Frontend Developer', 'Backend Developer', 'Full-stack Developer', 'Mobile App Developer', 'DevOps Engineer', 'Software Architect', 'UI/UX Designer', 'Software Engineer in Test', 'Game Developer', 'Embedded Systems Developer'],
+    'Cybersecurity': ['Information Security Analyst', 'Ethical Hacker', 'Security Consultant', 'Security Engineer', 'Chief Information Security Officer', 'Security Operations Center Analyst', 'Cryptographer', 'Incident Responder', 'Security Software Developer', 'Cybersecurity Researcher'],
+    'Data Science and Analytics': ['Data Scientist', 'Data Analyst', 'Machine Learning Engineer', 'Business Intelligence Analyst', 'Data Engineer', 'Statistician', 'Quantitative Analyst', 'Operations Analyst', 'Big Data Engineer', 'Data Visualization Specialist'],
+    'Network and Systems Administration': ['Network Administrator', 'Systems Administrator', 'Network Engineer', 'Systems Engineer', 'Cloud Administrator', 'IT Support Specialist', 'Database Administrator', 'Virtualization Engineer', 'Wireless Communication Engineer', 'IT Security Administrator'],
+    'IT Project Management': ['Project Manager', 'Scrum Master', 'Product Owner', 'IT Program Manager', 'Agile Coach', 'Business Analyst', 'Release Manager', 'Quality Assurance Manager', 'IT Service Manager', 'Change Management Specialist']
+    // Add the other categories here...
+};
+
+
+const types = ['Software Development', 'Cybersecurity', 'Data Science and Analytics', 'Network and Systems Administration', 'IT Project Management'];
 function TabGroup() {
     const [active, setActive] = useState(types[0]);
+    const [roleCards, setRoleCards] = useState(roles['Software Development']); // Initialize with the roles for 'Software Development'
+
+    const handleTabClick = (type) => {
+        setActive(type);
+
+        // Update roleCards state with the roles for the clicked tab
+        setRoleCards(roles[type]);
+    };
+
+
     return (
         <>
             <ButtonGroup className='mx-auto w-75'>
@@ -43,15 +66,21 @@ function TabGroup() {
                     <Tab
                         key={type}
                         active={active === type}
-                        onClick={() => setActive(type)}
+                        onClick={() => handleTabClick(type)}
                     >
                         {type}
                     </Tab>
                 ))}
             </ButtonGroup>
             <p />
-            <p> Your payment selection: {active} </p>
-            <RoleCard />
+            <p> Your category selection: {active} </p>
+            <div className="mx-auto w-75" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start' }}>
+                {roleCards.map((roleCard, index) => (
+                    <div key={index} style={{ width: '17%', margin: '10px' }}>
+                        <RoleCard roleName={roleCard} />
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
