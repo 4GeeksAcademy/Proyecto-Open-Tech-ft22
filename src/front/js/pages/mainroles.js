78807433ba7ad01d39a29ad30f11cc62c2c1600b
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-27 17:37:40 +0000

    agregue tabs de react

--- src/front/js/pages/mainroles.js
+++ src/front/js/pages/mainroles.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RoleCard from '../component/roleCard';
+import styled from 'styled-components';
 
 function MainRoles() {
     return (
         <div>
-            <RoleCard />
+            <h1>MainComponent</h1>
+            <TabGroup />
         </div>
     );
 }
 
 export default MainRoles;
+
+
+
+const Tab = styled.button`
+  font-size: 20px;
+  padding: 10px 60px;
+  cursor: pointer;
+  opacity: 0.6;
+  background: white;
+  border: 0;
+  outline: 0;
+  ${({ active }) =>
+        active &&
+        `
+    border-bottom: 2px solid black;
+    opacity: 1;
+  `}
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+const types = ['Cash', 'Credit Card', 'Bitcoin'];
+function TabGroup() {
+    const [active, setActive] = useState(types[0]);
+    return (
+        <>
+            <ButtonGroup className='mx-auto w-75'>
+                {types.map(type => (
+                    <Tab
+                        key={type}
+                        active={active === type}
+                        onClick={() => setActive(type)}
+                    >
+                        {type}
+                    </Tab>
+                ))}
+            </ButtonGroup>
+            <p />
+            <p> Your payment selection: {active} </p>
+            <RoleCard />
+        </>
+    );
+}
+// Usage
+<TabGroup />
