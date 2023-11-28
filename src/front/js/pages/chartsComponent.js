c9433406d17ad6a87c027d7d4bbfe7bf9d120eb3
Fernando Castillo <122224880+fcastillor90@users.noreply.github.com>
2023-11-28 13:39:14 +0000

    graficooos yuuju

--- src/front/js/pages/chartsComponent.js
+++ src/front/js/pages/chartsComponent.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, { useState } from 'react';
+import { Chart as ChartJs } from 'chart.js';
+import { Bar, Doughnut, Line } from 'react-chartjs-2';
+
+import sourceData from "./data/sourceData.json";
+
 
 export const ChartsComponent = () => {
+    const [labels, setLabels] = useState(sourceData.map((data) => data.label))
+    const [values, setValues] = useState(sourceData.map((data) => data.value))
+
     // Your component logic here
 
     const data = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-            {
-                label: 'Example Dataset',
-                data: [12, 19, 8, 5, 10, 3],
-                backgroundColor: 'rgba(75,192,192,0.2)',
-                borderColor: 'rgba(75,192,192,1)',
-                borderWidth: 1,
-            },
-        ],
-    };
+        labels: labels,
+        datasets: [{
+            label: "My First Dataset",
+            data: values,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
+        }]
+    }
 
     const options = {
         scales: {
@@ -32,8 +53,14 @@ export const ChartsComponent = () => {
 
     return (
         <div>
-            <h1>ChartsComponent</h1>
-            <Bar data={data} options={options} />
+            <div>
+                <h1>ChartsComponent</h1>
+                <Bar
+                    data={data} />
+            </div>
+            <div>char 2</div>
+            <div>char 3</div>
+
         </div>
     );
 
