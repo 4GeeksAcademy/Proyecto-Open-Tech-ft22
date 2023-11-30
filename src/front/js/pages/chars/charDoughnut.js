5b84d3a7cf6a65e9cbf63c6cbca6803b3899b0cb
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-30 15:33:40 +0000

    colores lindos

--- src/front/js/pages/chars/charDoughnut.js
+++ src/front/js/pages/chars/charDoughnut.js
@@ -21,22 +21,28 @@ export const ChartDoughnut = ({ category, roles, salaries }) => {
             label: "Grafico por el poder Perruno",
             data: values,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)'
+                /*Azul*/ 'rgba(15, 15, 255, 0.2)',
+                /*Morado*/ 'rgba(139, 15, 255, 0.2)',
+                /*Menos morado*/ 'rgba(202, 10, 255, 0.2)',
+                /*Rosado*/ 'rgba(255, 5, 243, 0.2)',
+                /*Fucsia*/ 'rgba(250, 0, 104, 0.2)',
+                /*Sandia*/ 'rgba(245, 0, 37, 0.2)',
+                /*Naranja*/ 'rgba(240, 24, 0, 0.2)',
+                /*Salmon*/ 'rgba(235, 86, 0, 0.2)',
+                /*Gold*/ 'rgba(230, 145, 0, 0.2)',
+                /*Amarillo*/ 'rgba(224, 202, 0, 0.2)',
             ],
             borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)',
-                'rgb(153, 102, 255)',
-                'rgb(201, 203, 207)'
+                /*Azul*/ 'rgb(15, 15, 255)',
+                /*Morado*/ 'rgb(139, 15, 255)',
+                /*Menos morado*/ 'rgb(202, 10, 255)',
+                /*Rosado*/ 'rgb(255, 5, 243)',
+                /*Fucsia*/ 'rgb(250, 0, 104)',
+                /*Sandia*/ 'rgb(245, 0, 37)',
+                /*Naranja*/ 'rgb(240, 24, 0)',
+                /*Salmon*/ 'rgb(235, 86, 0)',
+                /*Gold*/ 'rgb(230, 145, 0)',
+                /*Amarillo*/ 'rgb(224, 202, 0)'
             ],
             borderWidth: 1
         }]
