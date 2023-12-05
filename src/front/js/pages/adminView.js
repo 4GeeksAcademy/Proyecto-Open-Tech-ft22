c1b50de135496b18a9ce414244805330e3adfc81
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-12-05 22:06:26 +0000

    se verifica luego de presionar boton

--- src/front/js/pages/adminView.js
+++ src/front/js/pages/adminView.js
@@ -1,19 +1,31 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../store/appContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faEye } from '@fortawesome/free-solid-svg-icons';
 import "../../styles/admin.css";
 
 
 const AdminView = () => {
     const { store } = useContext(Context);
     const [data, setData] = useState([]);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [isPdfVerified, setPdfVerified] = useState(false);
 
     useEffect(() => {
         fetch(`${store.apiURL}/api/salary`)
             .then(response => response.json())
             .then(data => setData(data));
     }, []);
+
+    const handleVerifyClick = () => {
+        // Update the verification status of the selected item
+        const updatedItem = { ...selectedItem, isVerified: true };
+        setSelectedItem(updatedItem);
+
+        // Update the data array
+        const updatedData = data.map(item => item === selectedItem ? updatedItem : item);
+        setData(updatedData);
+    };
 
     return (
         <div>
@@ -28,37 +40,35 @@ const AdminView = () => {
                             <th>Country</th>
                             <th>Amount</th>
                             <th>Verified</th>
+                            <th>PDF view</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {data && data.sort((a, b) => a.years_of_experience - b.years_of_experience).map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.years_of_experience}</td>
-                                <td>{item.role}</td>
-                                <td>{item.category}</td>
-                                <td>{item.city}</td>
-                                <td>{item.country}</td>
-                                <td>{item.amount}</td>
-                                <td>{item.pdf ? <FontAwesomeIcon icon={faCheckCircle} style={{ color: '#ba4ff8', marginLeft: '20px' }} /> : null}</td>
-                            </tr>
-                        ))}
+                        {data && data
+                            .filter(item => item.pdf)
+                            .sort((a, b) => a.years_of_experience - b.years_of_experience)
+                            .map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.years_of_experience}</td>
+                                    <td>{item.role}</td>
+                                    <td>{item.category}</td>
+                                    <td>{item.city}</td>
+                                    <td>{item.country}</td>
+                                    <td>{item.amount}</td>
+                                    <td>{item.isVerified ? <FontAwesomeIcon icon={faCheckCircle} style={{ color: '#ba4ff8', marginLeft: '20px' }} /> : null}</td>
+                                    <td>{item.pdf ? <FontAwesomeIcon icon={faEye} style={{ color: '#ba4ff8', marginLeft: '20px' }} onClick={() => setSelectedItem(item)} /> : null}</td>
+                                </tr>
+                            ))}
                     </tbody>
                 </table>
             </div>
 
 
 
-            <div>
-                <h5>PDF 1</h5>
-                <iframe className="myPDF" src="https://res.cloudinary.com/dnwpecmun/image/upload/f_auto,q_auto/v1/salaries/pnfvzprvrifcqelwsmif"></iframe>
-            </div>
-            <div>
-                <h5>PDF 2</h5>
-                <iframe className="myPDF" src="https://res.cloudinary.com/dnwpecmun/image/upload/f_auto,q_auto/v1/salaries/pnfvzprvrifcqelwsmif"></iframe>
-            </div>
-            <div>
-                <h5>PDF 3</h5>
-                <iframe className="myPDF" src="https://res.cloudinary.com/dnwpecmun/image/upload/f_auto,q_auto/v1/salaries/pnfvzprvrifcqelwsmif"></iframe>
+            <div class="pdf-container">
+                <h3>Selected PDF:</h3>
+                {selectedItem && <div className='justify-content-center verify-div'><button className='verify-button' onClick={handleVerifyClick}>Verify PDF <FontAwesomeIcon icon={faCheckCircle} style={{ color: '#ba4ff8', marginLeft: '20px' }} /></button></div>}
+                {selectedItem ? <iframe class="myPDF" src={selectedItem.pdf_optimized}></iframe> : <p>Please select a PDF from the table by clicking on the <FontAwesomeIcon icon={faEye} /> icon.</p>}
             </div>
         </div>
     );
