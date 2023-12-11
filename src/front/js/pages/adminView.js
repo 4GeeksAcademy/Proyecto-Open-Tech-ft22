afde41140305129498d7fd6598ec761a2dbb59fb
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-12-11 14:59:31 +0000

    email listo

--- src/front/js/pages/adminView.js
+++ src/front/js/pages/adminView.js
@@ -6,6 +6,7 @@ import { Button, Modal } from 'react-bootstrap';
 import "../../styles/admin.css";
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import emailjs from 'emailjs-com';
 
 
 const AdminView = () => {
@@ -68,81 +69,101 @@ const AdminView = () => {
             }
 
             const updatedData = data.map(item => item.id === selectedItem.id ? updatedItem : item);
-                    setShowModal(false);
-                    setData(updatedData);
-                    setSelectedItem(null);
 
-                    // Re-fetch the data
-                    await fetchData();
-                } catch (error) {
-                    console.error(error);
-                }
+            const templateParams = {
+                to_name: store.user.name, // Replace with the user's name
+                to_email: store.user.email, // Replace with the useremail
+                message: 'Your PDF has been verified, thank you for contributing to salary transparency.', // Replace with your message
+            };
+            // Send the email
+            emailjs.send('service_9es8ikt', 'template_f3tsjwc', templateParams, 'Zjop8ResQtm_ssC2J')
+
+
+            setShowModal(false);
+            setData(updatedData);
+            setSelectedItem(null);
+
+            // Re-fetch the data
+            await fetchData();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handleReject = async () => {
+        // Aqui agregar logica para rechazar PDF:
+        // Enviar correo de rechazo
+        // Dejar sin icono de verificacion en la vista del usuario
+        const updatedItem = { ...selectedItem, is_verified: false };
+        setSelectedItem(updatedItem);
+
+        // Send a request to the API to update the item
+        try {
+            const response = await fetch(`${store.apiURL}/api/salary/${selectedItem.id}/reject`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedItem),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to reject PDF');
             }
 
-            const handleReject = async () => {
-                // Aqui agregar logica para rechazar PDF:
-                // Enviar correo de rechazo
-                // Dejar sin icono de verificacion en la vista del usuario
-                const updatedItem = { ...selectedItem, is_verified: false };
-                setSelectedItem(updatedItem);
 
-                // Send a request to the API to update the item
-                try {
-                    const response = await fetch(`${store.apiURL}/api/salary/${selectedItem.id}/reject`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(updatedItem),
-                    });
+            const updatedData = data.map(item => item.id === selectedItem.id ? updatedItem : item);
+            const templateParams = {
+                to_name: store.user.name, // Replace with the user's name
+                to_email: store.user.email, // Replace with the useremail
+                message: 'Your PDF has been rejected, please make sure to select the right file.', // Replace with your message
+            };
+            // Send the email
+            emailjs.send('service_9es8ikt', 'template_f3tsjwc', templateParams, 'Zjop8ResQtm_ssC2J')
 
-                    if (!response.ok) {
-                        throw new Error('Failed to reject PDF');
-                    }
 
-                    const updatedData = data.map(item => item.id === selectedItem.id ? updatedItem : item);
-                    setShowRejectModal(false);
-                    setData(updatedData);
-                    setSelectedItem(null);
+            setShowRejectModal(false);
+            setData(updatedData);
+            setSelectedItem(null);
 
-                    // Re-fetch the data
-                    await fetchData();
-                } catch (error) {
-                    console.error(error);
-                }
-            }
+            // Re-fetch the data
+            await fetchData();
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-            return (
-                <div>
-                    <div className="text-center">
-                        <Link to='/dashboard' className='backButton'>{t('Go back to the main page')}</Link>
-                    </div>
-                    <div>
-                        <h4 style={{ marginLeft: '80px', color: 'white', marginTop: '30px' }}>{t('Pending')}:</h4>
-                        <table className="table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>{t('ID')}</th>
-                                    <th>{t('Years of experience')}</th>
-                                    <th>{t('Role')}</th>
-                                    <th>{t('City')}</th>
-                                    <th>{t('Country')}</th>
-                                    <th>{t('Amount')}</th>
-                                    <th>{t('To be Verified')}</th>
-                                    <th>{t('view PDF')}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data && data
-                                    .filter(item => item.pdf && !item.is_in_history)
-                                    .sort((a, b) => a.id - b.id)
-                                    .map((item, index) => (
-                                        <tr key={index}>
-                                            <td className="id-column">{t(item.id)}</td>
-                                            <td>{item.years_of_experience}</td>
-                                            <td>{t(item.role)}</td>
-                                            <td>{item.city}</td>
-                                            <td>{item.country}</td>
+    return (
+        <div>
+            <div className="text-center">
+                <Link to='/dashboard' className='backButton'>{t('Go back to the main page')}</Link>
+            </div>
+            <div>
+                <h4 style={{ marginLeft: '80px', color: 'white', marginTop: '30px' }}>{t('Pending')}:</h4>
+                <table className="table-bordered">
+                    <thead>
+                        <tr>
+                            <th>{t('ID')}</th>
+                            <th>{t('Years of experience')}</th>
+                            <th>{t('Role')}</th>
+                            <th>{t('City')}</th>
+                            <th>{t('Country')}</th>
+                            <th>{t('Amount')}</th>
+                            <th>{t('To be Verified')}</th>
+                            <th>{t('view PDF')}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data && data
+                            .filter(item => item.pdf && !item.is_in_history)
+                            .sort((a, b) => a.id - b.id)
+                            .map((item, index) => (
+                                <tr key={index}>
+                                    <td className="id-column">{t(item.id)}</td>
+                                    <td>{item.years_of_experience}</td>
+                                    <td>{t(item.role)}</td>
+                                    <td>{item.city}</td>
+                                    <td>{item.country}</td>
                                     <td>{item.amount}</td>
                                     <td>
                                         {item.is_verified
