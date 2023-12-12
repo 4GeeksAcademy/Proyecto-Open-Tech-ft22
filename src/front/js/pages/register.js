1f9c8a08a70a0484469a0db5194f1bf342a183c5
Fernando Castillo <122224880+fcastillor90@users.noreply.github.com>
2023-12-12 19:11:31 +0000

    register

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
+import "../../styles/register.css"
 
 export const Register = () => {
     const navigate = useNavigate();
@@ -22,22 +23,23 @@ export const Register = () => {
     };
 
     return (
-        <div className="pseudo-navbar-register" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div className="trolola pseudo-navbar-register" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+
             <h1 style={{ background: '-webkit-linear-gradient(left, blue, white)', WebkitBackgroundClip: 'text', color: 'transparent', paddingTop: '20px' }}>
                 {t('Registration')}
             </h1>
-            <form onSubmit={handleSubmit} className='mx-auto p-3 border-1 w-25' style={{ boxShadow: '0 5px 9px rgba(0, 0, 0, 0.5)', marginTop: '20px' }}>
+            <form onSubmit={handleSubmit} className='trolole mx-auto p-3 border-1' style={{ boxShadow: '0 5px 9px rgba(0, 0, 0, 0.5)', marginTop: '20px' }}>
                 <div className="form-group mb-3">
                     <label htmlFor="name" className="form-label" style={{ color: 'white' }}>{t('Name')}:</label>
-                    <input type="text" className="form-control" id="name" name="name" onChange={actions.handleChange} value={store.name} required autocomplete="off"/>
+                    <input type="text" className="form-control" id="name" name="name" onChange={actions.handleChange} value={store.name} required autocomplete="off" />
                 </div>
                 <div className="form-group mb-3">
                     <label htmlFor="username" className="form-label" style={{ color: 'white' }}>{t('Username')}:</label>
-                    <input type="text" className="form-control" id="username" name="username" onChange={actions.handleChange} value={store.username} required autocomplete="off"/>
+                    <input type="text" className="form-control" id="username" name="username" onChange={actions.handleChange} value={store.username} required autocomplete="off" />
                 </div>
                 <div className="form-group mb-3">
                     <label htmlFor="email" className="form-label" style={{ color: 'white' }}>{t('Email')}:</label>
-                    <input type="email" className="form-control" id="email" name="email" onChange={actions.handleChange} value={store.email} required autocomplete="off"/>
+                    <input type="email" className="form-control" id="email" name="email" onChange={actions.handleChange} value={store.email} required autocomplete="off" />
                 </div>
                 <div className="form-group mb-3">
                     <label htmlFor="password" className="form-label" style={{ color: 'white' }}>{t('Password')}:</label>
@@ -57,6 +59,7 @@ export const Register = () => {
 
                 <button type="submit" className='btn btn-primary w-100' style={{ backgroundColor: '#4f89ee' }}>{t('Sign up')}</button>
             </form>
+ 
             <p className='mt-3' style={{ fontSize: '14px', color: 'white' }}>
                 {t('Already have an account with us?')}{' '}
                 <Link to="/" style={{ color: '#4f89ee', textDecoration: 'underline' }}>
